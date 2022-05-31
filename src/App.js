f75a791a63aa03bf139715e47c0e204498ba32c1
Arman <armanmatevosyan470124@gmail.com>
2022-05-31 10:45:17 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./styles.css";
+
+const API_URL =
+  "https://api-mobilespecs.azharimm.site/v2/brands/apple-phones-48?page=2 ";
+
+async function api() {
+  let response = await fetch(API_URL);
+  let user = await response.json();
+  console.log(user.data.phones[0]);
+}
+
+// useEffect((async) => )
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    api();
+  }, []);
+  return <button className="btn btn-primary m-4 ">asd</button>;
 }
 
 export default App;
